b2b0ca7659dd2c2acdda6a7309b70435bd5a5368
Nitin Agarwal <nitinagarwal@Nitins-MacBook-Air.local>
2024-01-18 00:26:50 +0530

    Adding User Repository for interaction with User Table

---
+++ src/main/java/org/example/messagingserver/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.messagingserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.example.messagingserver.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
