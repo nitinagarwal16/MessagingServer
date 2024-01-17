f8919d3f645ea84f4ffe2a1e27c4998092edd37e
Nitin Agarwal <nitinagarwal@Nitins-MacBook-Air.local>
2024-01-18 00:16:59 +0530

    Adding Lombok dependency and creating User and Message Entities

---
+++ src/main/java/org/example/messagingserver/entity/Message.java
@@ -0,0 +1,51 @@
+package org.example.messagingserver.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User receiver;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private boolean read;
+
+    public Message() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Message(final User sender, final User receiver, final String text) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.text = text;
+        this.createdAt = LocalDateTime.now();
+        this.read = false;
+    }
+}
+
---
+++ src/main/java/org/example/messagingserver/entity/User.java
@@ -0,0 +1,35 @@
+package org.example.messagingserver.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String passcode;
+
+    public User() {
+
+    }
+
+    public User(final String username, final String passcode) {
+        this.username = username;
+        this.passcode = passcode;
+    }
+}
